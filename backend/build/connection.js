1f9ae5527e35606898193e96ca045e65f1d22e78
JuanDeDiosRamirezCampos <99991865+JuanDeDiosRamirezCampos@users.noreply.github.com>
2024-10-14 17:09:05 -0600

    Update connection.js

--- backend/build/connection.js
+++ backend/build/connection.js
@@ -5,7 +5,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const promise_mysql_1 = __importDefault(require("promise-mysql"));
 const pool = promise_mysql_1.default.createPool({
-    host: 'sql.freesqldatabase.com',
+    host: 'sql3.freesqldatabase.com',
     user: 'sql3737832',
     password: '26RYgsSb1h',
     database: 'sql3737832',
